dcbd267ca4919ad3f43c808a1985699aa328e7af
Mintty92 <andylincourt@gmail.com>
2022-02-01 18:18:18 -0500

    Updates

---
+++ precache-manifest.219119d15a6af071a7043c68b985ae16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a089cf3c7f7a29662665b01373b7c5a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e63e53c20fe4ad91979",
+    "url": "/monsters-rolodex/static/css/main.deacce3b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e63e53c20fe4ad91979",
+    "url": "/monsters-rolodex/static/js/main.914c3902.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
